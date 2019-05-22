e53f8bf39d374d2a16cde955feeb644a8b2eb70f
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2019-05-22 15:32:03 +0200

    adding CString Formater utility
    Signed-off-by: Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>

---
+++ includes/utilities/CStringFormater.hpp
@@ -0,0 +1,28 @@
+#ifndef C_STRING_FORMATER_UTILITY_HPP
+#define C_STRING_FORMATER_UTILITY_HPP
+
+#include <cstring>
+#include <cctype>
+
+constexpr unsigned int hash(const char *str, int h = 0)
+{
+    return !str[h] ? 5381 : (hash(str, h + 1) * 33) ^ str[h];
+}
+
+void convertToUpperCase(char *input)
+{
+    while (*input != '\0')
+    {
+        if (islower(*input))
+            *input = toupper(*input);
+    }
+}
+
+void copyCharArray(char from[], char to[])
+{
+    unsigned int stringLenght = strlen(from);
+    strncpy(to, from, stringLenght);
+    from[stringLenght] = '\0';
+}
+
+#endif //C_STRING_FORMATER_UTILITY_HPP
