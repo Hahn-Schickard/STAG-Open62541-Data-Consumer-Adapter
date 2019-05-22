07d28ead4635a3a4885f70bf4ebc00b4935b7478
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2019-05-22 15:54:14 +0200

    fixed NodeInformation struct names
    Signed-off-by: Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>

--- tests/unit_tests/NodeInformation/NodeInformationTests.cpp
+++ tests/unit_tests/NodeInformation/NodeInformationTests.cpp
@@ -2,12 +2,11 @@
 #include "CStringFormater.hpp"
 
 #include <gtest/gtest.h>
-#include <string>
 
 #define MAX_BASE_NODE_DESCRITPOR_TEST_COUNT 3
 #define BASE_NODE_DESCRIPTOR_FIELD_COUNT 7
 
-constchar *baseNodeDescriptorInformation[MAX_BASE_NODE_DESCRITPOR_TEST_COUNT][BASE_NODE_DESCRIPTOR_FIELD_COUNT] = {
+const char *baseNodeDescriptorInformation[MAX_BASE_NODE_DESCRITPOR_TEST_COUNT][BASE_NODE_DESCRIPTOR_FIELD_COUNT] = {
     {"UNRECOGNIZED_NODE", "false", "en-US", "1", "Base Node Descriptor Test 1", "base_node_descriptor_test_1", "This is some sort of a description"},
     {"UNRECOGNIZED_NODE", "true", "en-US", "2", "Base Node Descriptor Test 2", "base_node_descriptor_test_2", "This is some sort of a description"},
     {"VARIABLE_NODE", "false", "en-US", "3", "Base Node Descriptor Test 3", "base_node_descriptor_test_3", "This is some sort of a description"}};
@@ -35,12 +34,12 @@ bool identifyWritableFlag(const char *writableFlag)
     return (strcmp("TRUE", writableFlag) || strcmp("Y", writableFlag)) ? true : false;
 }
 
-BaseNodeDescriptor setUpBaseNodeDescriptor(const char *nodeClassType, const char *writableFlag,
-                                           const char *locale, const char *uniqueId, const char *displayName,
-                                           const char *browseName, const char *description)
+BaseNodeDescription setUpBaseNodeDescriptor(const char *nodeClassType, const char *writableFlag,
+                                            const char *locale, const char *uniqueId, const char *displayName,
+                                            const char *browseName, const char *description)
 {
-    BaseNodeDescriptor baseNodeDescritpor;
-    baseNodeDescritpor.nodeClassType = identifyNodeClassType(nodeClassType);
+    BaseNodeDescription baseNodeDescritpor;
+    baseNodeDescritpor.nodeClass = identifyNodeClassType(nodeClassType);
     baseNodeDescritpor.writableFlag = identifyWritableFlag(writableFlag);
     copyCharArray(locale, baseNodeDescritpor.locale);
     copyCharArray(uniqueId, baseNodeDescritpor.uniqueId);
@@ -55,7 +54,7 @@ TEST(NodeInformationTests, BaseNodeDescriptorContentFormatTest)
 {
     for (int i = 0; i < MAX_BASE_NODE_DESCRITPOR_TEST_COUNT; i++)
     {
-        BaseNodeDescriptor baseNodeDescriptor = setUpBaseNodeDescriptor(
+        BaseNodeDescription baseNodeDescriptor = setUpBaseNodeDescriptor(
             baseNodeDescriptorInformation[i][0],
             baseNodeDescriptorInformation[i][1],
             baseNodeDescriptorInformation[i][2],
@@ -64,7 +63,7 @@ TEST(NodeInformationTests, BaseNodeDescriptorContentFormatTest)
             baseNodeDescriptorInformation[i][5],
             baseNodeDescriptorInformation[i][6]);
 
-        EXPECT_EQ(identifyNodeClassType(baseNodeDescriptorInformation[i][0]), baseNodeDescriptor.nodeClassType);
+        EXPECT_EQ(identifyNodeClassType(baseNodeDescriptorInformation[i][0]), baseNodeDescriptor.nodeClass);
         EXPECT_EQ(identifyWritableFlag(baseNodeDescriptorInformation[i][1]), baseNodeDescriptor.writableFlag);
 
         EXPECT_STREQ(baseNodeDescriptorInformation[i][2], baseNodeDescriptor.locale);
