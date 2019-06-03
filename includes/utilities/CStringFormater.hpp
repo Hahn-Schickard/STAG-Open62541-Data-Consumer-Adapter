64449e1df17d7ceb7c2a4658770032fe1df050d3
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2019-06-03 17:38:20 +0200

    adding boolean identification from string
    Signed-off-by: Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>

--- includes/utilities/CStringFormater.hpp
+++ includes/utilities/CStringFormater.hpp
@@ -14,13 +14,14 @@ constexpr unsigned int hash(const char *str, int h = 0)
 /*
  * Returns a malloced pointer! 
  * DO NOT FORGET TO USE FREE() ! ! ! 
- */ 
-char* convertToUpperCase(const char *input)
+ */
+char *convertToUpperCase(const char *input)
 {
     unsigned int stringLenght = strlen(input);
-    char *output = (char* )malloc(sizeof(char) * stringLenght);
-    for(int i= 0; i < stringLenght; i++){
-        if(islower(input[i]))
+    char *output = (char *)malloc(sizeof(char) * stringLenght);
+    for (int i = 0; i < stringLenght; i++)
+    {
+        if (islower(input[i]))
             output[i] = toupper(input[i]);
         output[i] = input[i];
     }
@@ -30,8 +31,9 @@ char* convertToUpperCase(const char *input)
 void convertToUpperCase(char *input)
 {
     unsigned int stringLenght = strlen(input);
-    for(int i= 0; i < stringLenght; i++){
-        if(islower(input[i]))
+    for (int i = 0; i < stringLenght; i++)
+    {
+        if (islower(input[i]))
             input[i] = toupper(input[i]);
     }
 }
@@ -41,6 +43,16 @@ void copyCharArray(const char from[], char to[])
     unsigned int stringLenght = strlen(from);
     strncpy(to, from, stringLenght);
     to[stringLenght] = '\0';
+}
+
+bool identifyBoolean(const char *booleanValue)
+{
+    char *processedValue = convertToUpperCase(booleanValue);
+    bool booleanFlag =
+        (strcmp("TRUE", processedValue) || strcmp("Y", processedValue)) ? true
+                                                                        : false;
+    free(processedValue);
+    return booleanFlag;
 }
 
 #endif //C_STRING_FORMATER_UTILITY_HPP
