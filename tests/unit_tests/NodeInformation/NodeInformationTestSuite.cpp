3515842b3834d4eedd0e1daa2b21ab97d275da91
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2019-06-05 16:00:34 +0200

    renamed NodeInforamtionTests into NodeInformationTestSuite
    Signed-off-by: Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>

---
+++ tests/unit_tests/NodeInformation/NodeInformationTestSuite.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include "BaseNodeDescriptorTests.hpp"
+#include "VariableNodeDescriptorTests.hpp"
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
