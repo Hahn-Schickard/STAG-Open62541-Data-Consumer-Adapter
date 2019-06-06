6f8edecc3ca3b4a886a2cecad79b17fff6ae5304
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2019-06-06 15:51:55 +0200

    added exception handling for filestreams
    Signed-off-by: Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>

--- tests/unit_tests/NodeInformation/NodeDescriptorsTestUtils.hpp
+++ tests/unit_tests/NodeInformation/NodeDescriptorsTestUtils.hpp
@@ -8,8 +8,16 @@ template <typename T>
 T getNodeDescription(const char *node_descriptor_name, std::string test_name)
 {
     int test_number = std::stoi(test_name.substr(test_name.find("/") + 1));
+    T node_descriptor;
 
-    T node_descriptor = nlohmann::setUpNodeDescriptors<T>(node_descriptor_name)[test_number];
+    try
+    {
+        node_descriptor = nlohmann::setUpNodeDescriptors<T>(node_descriptor_name)[test_number];
+    }
+    catch (std::exception &ex)
+    {
+        std::cerr << "Cought a JSON exception for file: " << node_descriptor_name << "for test: " << test_name << " " << ex.what() << std::endl;
+    }
 
     return node_descriptor;
 }
@@ -25,7 +33,16 @@ std::string getCurrentTestName()
 template <typename T>
 std::vector<T> getNodeDescriptors(const char *node_descriptors_json_file)
 {
-    std::vector<T> node_descriptors = nlohmann::setUpNodeDescriptors<T>(node_descriptors_json_file);
+    std::vector<T> node_descriptors;
+    try
+    {
+        node_descriptors = nlohmann::setUpNodeDescriptors<T>(node_descriptors_json_file);
+    }
+    catch (std::exception &ex)
+    {
+        std::cerr << "Cought a JSON exception for file: " << node_descriptors_json_file << " " << ex.what() << std::endl;
+    }
+
     return node_descriptors;
 }
 
