a653c0af874779dcab81b3035bcff3d989c45f7f
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2019-06-11 13:28:30 +0200

    seperated into source and header files
    Signed-off-by: Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>

--- includes/utilities/CStringFormater.hpp
+++ includes/utilities/CStringFormater.hpp
@@ -1,58 +1,23 @@
-#ifndef C_STRING_FORMATER_UTILITY_HPP
-#define C_STRING_FORMATER_UTILITY_HPP
+#ifndef C_STRING_FORMATER_UTILITY_HPP_
+#define C_STRING_FORMATER_UTILITY_HPP_
 
-#include <stdio.h>
+#include <cctype>
 #include <cstdlib>
 #include <cstring>
-#include <cctype>
+#include <stdio.h>
 
-constexpr unsigned int hash(const char *str, int h = 0)
-{
-    return !str[h] ? 5381 : (hash(str, h + 1) * 33) ^ str[h];
-}
+constexpr unsigned int hash(const char *str, int h = 0);
 
 /*
- * Returns a malloced pointer! 
- * DO NOT FORGET TO USE FREE() ! ! ! 
+ * Returns a malloced pointer!
+ * DO NOT FORGET TO USE FREE() ! ! !
  */
-char *getMallocedUpperCase(const char *input)
-{
-    unsigned int string_lenght = strlen(input);
-    char *output = (char *)malloc(sizeof(char) * string_lenght);
-    for (int i = 0; i < string_lenght; i++)
-    {
-        if (islower(input[i]))
-            output[i] = toupper(input[i]);
-        output[i] = input[i];
-    }
-    return output;
-}
+char *getMallocedUpperCase(const char *input);
 
-void convertToUpperCase(char *input)
-{
-    unsigned int string_lenght = strlen(input);
-    for (int i = 0; i < string_lenght; i++)
-    {
-        if (islower(input[i]))
-            input[i] = toupper(input[i]);
-    }
-}
+void convertToUpperCase(char *input);
 
-void copyCharArray(const char from[], char to[])
-{
-    unsigned int string_lenght = strlen(from);
-    strncpy(to, from, string_lenght);
-    to[string_lenght] = '\0';
-}
+void copyCharArray(const char from[], char to[]);
 
-bool identifyBoolean(const char *boolean_value)
-{
-    char *processed_value = getMallocedUpperCase(boolean_value);
-    bool boolean_flag =
-        (strcmp("TRUE", processed_value) || strcmp("Y", processed_value)) ? true
-                                                                          : false;
-    free(processed_value);
-    return boolean_flag;
-}
+bool identifyBoolean(const char *boolean_value);
 
-#endif //C_STRING_FORMATER_UTILITY_HPP
+#endif // C_STRING_FORMATER_UTILITY_HPP_
