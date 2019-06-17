111eddadbab8b2919feeaa20e0a7c36647c1ee90
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2019-06-17 16:15:41 +0200

    moved code coverage tools to root, removed tools dir
    Signed-off-by: Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>

--- check_code_coverage.sh
+++ check_code_coverage.sh
@@ -1,4 +1,8 @@
 #!/bin/bash 
 
+if [ -f ./code_coverage.info ]
+    rm ./code_coverage.info 
+fi 
+
 lcov --directory ../build/ --capture --output-file ./code_coverage.info -rc lcov_branch_coverage=1
 genhtml code_coverage.info --branch-coverage --output-directory ./code_coverage_report/
