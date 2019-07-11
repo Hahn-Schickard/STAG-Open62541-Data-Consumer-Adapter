1a09e399507a8c36e446f53bd9ae45e2a3e57fd5
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2019-07-11 11:51:16 +0200

    compilable NodeBUilder implementation
    Signed-off-by: Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>

--- includes/NodeBuilder/NodeBuilder.hpp
+++ includes/NodeBuilder/NodeBuilder.hpp
@@ -4,9 +4,11 @@
 #include "NodeInformation.h"
 #include "device.hpp"
 
-namespace opcua_model_translator {
+namespace opcua_model_translator
+{
 
-class NodeBuilder {
+class NodeBuilder
+{
 public:
   NodeBuilder(Device *device);
   NodeDescription *get_Node_Description();
@@ -15,11 +17,14 @@ public:
 
 private:
   void add_Element_Groups();
-  template <typename T>
-  NodeDescription build_Node_Description(T *device_element);
-  NodeClassType identify_Element_Type(DeviceElementGroup *device_element);
+  NodeDescription build_Node_Description(DeviceElement *device_element);
+  void build_Node_Class(NodeDescription &element_node, DeviceElement *device_element);
+  void build_Variable_Node_Class(NodeDescription &element_node, DeviceElement *device_element);
+  void build_Method_Node_Class(NodeDescription &element_node, DeviceElement *device_element);
+  void build_Object_Node_Class(NodeDescription &element_node, DeviceElementGroup *device_element);
+  NodeClassType identify_Element_Type(DeviceElement *device_element);
   Device *device_base;
 };
-}
+} // namespace opcua_model_translator
 
 #endif //__NODE_BUILDER_HPP_
--- sources/NodeBuilder/NodeBuilder.cpp
+++ sources/NodeBuilder/NodeBuilder.cpp
@@ -3,7 +3,8 @@
 
 using namespace opcua_model_translator;
 
-NodeBuilder::NodeBuilder(Device *device) {
+NodeBuilder::NodeBuilder(Device *device)
+{
   device_base = device;
 
   copyCharArray(device_base->getElementName().c_str(),
@@ -19,70 +20,125 @@ NodeBuilder::NodeBuilder(Device *device) {
   add_Element_Groups();
 }
 
-void NodeBuilder::add_Element_Groups() {
+void NodeBuilder::add_Element_Groups()
+{
   std::vector<DeviceElementGroup *> device_element_groups =
       device_base->getDeviceElementGroups();
 
   device_node->object_node_descriptor.children_count =
       device_element_groups.size();
 
-  for (std::size_t i = 0; i < device_element_groups.size(); i++) {
-    DeviceElementGroup *device_element = device_element_groups[i];
-    copyCharArray(device_element->getElementName().c_str(),
-                  device_node->object_node_descriptor.children[i]
-                      .base_node_descriptor.display_name);
-    copyCharArray(device_node->object_node_descriptor.children[i]
-                      .base_node_descriptor.display_name,
-                  device_node->object_node_descriptor.children[i]
-                      .base_node_descriptor.browse_name);
-    copyCharArray("EN_US", device_node->object_node_descriptor.children[i]
-                               .base_node_descriptor.locale);
-    device_node->object_node_descriptor.children[i]
-        .base_node_descriptor.node_class =
-        identify_Element_Type(device_element);
-    copyCharArray(device_element->getElementDescription().c_str(),
-                  device_node->object_node_descriptor.children[i]
-                      .base_node_descriptor.description);
-    copyCharArray(device_element->getReferenceId().c_str(),
-                  device_node->object_node_descriptor.children[i]
-                      .base_node_descriptor.unique_id);
+  for (std::size_t i = 0; i < device_element_groups.size(); i++)
+  {
+    device_node->object_node_descriptor.children[i] = build_Node_Description(device_element_groups[i]);
   }
 }
 
-template <typename T>
-NodeDescription NodeBuilder::build_Node_Description(T *device_element) {
-  NodeDescription device_node;
-  // TODO: implement stub
-  return device_node;
+NodeDescription NodeBuilder::build_Node_Description(DeviceElement *device_element)
+{
+  NodeDescription element_node;
+
+  copyCharArray(device_element->getElementName().c_str(), element_node.base_node_descriptor.display_name);
+  copyCharArray(element_node.base_node_descriptor.display_name, element_node.base_node_descriptor.browse_name);
+  copyCharArray("EN_US", element_node.base_node_descriptor.locale);
+  element_node.base_node_descriptor.node_class = identify_Element_Type(device_element);
+  copyCharArray(device_element->getElementDescription().c_str(), element_node.base_node_descriptor.description);
+  copyCharArray(device_element->getReferenceId().c_str(), element_node.base_node_descriptor.unique_id);
+  build_Node_Class(element_node, device_element);
+
+  return element_node;
+}
+
+void NodeBuilder::build_Node_Class(NodeDescription &element_node, DeviceElement *device_element)
+{
+  switch (element_node.base_node_descriptor.node_class)
+  {
+  case NodeClassType::VARIABLE_NODE:
+  {
+    build_Variable_Node_Class(element_node, device_element);
+    break;
+  }
+  case NodeClassType::METHOD_NODE:
+  {
+    build_Method_Node_Class(element_node, device_element);
+    break;
+  }
+  case NodeClassType::OBJECT_NODE:
+  {
+    build_Object_Node_Class(element_node, (DeviceElementGroup *)device_element);
+    break;
+  }
+  case NodeClassType::UNRECOGNIZED_NODE:
+  default:
+  {
+    // TODO: Handle default behaviour
+    break;
+  }
+  }
+}
+
+void NodeBuilder::build_Variable_Node_Class(NodeDescription &element_node, DeviceElement *device_element)
+{
+  // TODO: wait until valueDataType has been made accessable.
+  //element_node.variable_node_descriptor.data.type = device_element->getMetricValueType();
+}
+
+void NodeBuilder::build_Method_Node_Class(NodeDescription &element_node, DeviceElement *device_element)
+{
+  // TODO: handle method building
+}
+
+void NodeBuilder::build_Object_Node_Class(NodeDescription &element_node, DeviceElementGroup *device_element)
+{
+  std::vector<DeviceElement *> device_subelements =
+      device_element->getSubElements();
+  std::vector<DeviceElementGroup *> debice_subgroups = device_element->getSubElementGroups();
+  element_node.object_node_descriptor.children_count = debice_subgroups.size() + debice_subgroups.size();
+  for (std::size_t i = 0; i < device_subelements.size(); i++)
+  {
+    element_node.object_node_descriptor.children[i] = build_Node_Description(device_subelements[i]);
+  }
+  for (std::size_t i = device_subelements.size(); i < element_node.object_node_descriptor.children_count; i++)
+  {
+    element_node.object_node_descriptor.children[i] = build_Node_Description(debice_subgroups[i]);
+  }
 }
 
 NodeClassType
-NodeBuilder::identify_Element_Type(DeviceElementGroup *device_element) {
+NodeBuilder::identify_Element_Type(DeviceElement *device_element)
+{
   NodeClassType node_type;
   ElementType element_type = device_element->getElementType();
-  switch (element_type) {
-  case Group: {
+  switch (element_type)
+  {
+  case Group:
+  {
+    node_type = NodeClassType::OBJECT_NODE;
+    break;
+  }
+  case Readonly:
+  {
     node_type = NodeClassType::VARIABLE_NODE;
     break;
   }
-  case Readonly: {
+  case Observable:
+  {
     node_type = NodeClassType::VARIABLE_NODE;
     break;
   }
-  case Observable: {
+  case Writable:
+  {
     node_type = NodeClassType::VARIABLE_NODE;
     break;
   }
-  case Writable: {
-    node_type = NodeClassType::VARIABLE_NODE;
-    break;
-  }
-  case Function: {
+  case Function:
+  {
     node_type = NodeClassType::METHOD_NODE;
     break;
   }
   case Undefined:
-  default: {
+  default:
+  {
     node_type = NodeClassType::UNRECOGNIZED_NODE;
     break;
   }
