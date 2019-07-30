4cded3e695e0c8c1ed40db499e5640fa3454ab71
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2019-07-30 13:17:54 +0200

    synced with template v0.6.0
    Signed-off-by: Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,4 +1,5 @@
-if(NOT IS_SUBMODULE_PROJECT)
+if(NOT IS_SUBMODULE_PROJECT AND CMAKE_BUILD_TYPE STREQUAL "Debug")
+#====================================== JSON SERIALIZER DEPENDENCY ==========================================
     message(STATUS "Configuring external dependencies")
     configure_file(JsonDependency.txt json-download/CMakeLists.txt)
     execute_process(COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" .
@@ -9,23 +10,27 @@ if(NOT IS_SUBMODULE_PROJECT)
     set(JSON_INCLUDES "${PROJECT_SOURCE_DIR}/libs/json-src/single_include/")
     set(JSON_BuildTests OFF CACHE INTERNAL "")
 
-    option(INSTALL_GMOCK "Install Googletest's GMock?" OFF)
-    option(INSTALL_GTEST "Install Googletest's GTest?" OFF)
-    configure_file(GoogleTestDependency.txt googletest-download/CMakeLists.txt)
-    execute_process(COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" .
-        WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/googletest-download" )
-    execute_process(COMMAND "${CMAKE_COMMAND}" --build .
-        WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/googletest-download" )
+#======================================== GOOGLE TEST DEPENDENCY ============================================
+    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+        option(INSTALL_GMOCK "Install Googletest's GMock?" OFF)
+        option(INSTALL_GTEST "Install Googletest's GTest?" OFF)
 
-    set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+        configure_file(GoogleTestDependency.txt googletest-download/CMakeLists.txt)
+        execute_process(COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" .
+            WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/googletest-download" )
+        execute_process(COMMAND "${CMAKE_COMMAND}" --build .
+            WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/googletest-download" )
 
-    add_subdirectory("${PROJECT_SOURCE_DIR}/tests/googletest-src"
-                    "${CMAKE_BINARY_DIR}/googletest-build")
+        set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 
-    if(CMAKE_VERSION VERSION_LESS 2.8.11)
-        include_directories("${gtest_SOURCE_DIR}/include"
-                            "${gmock_SOURCE_DIR}/include")
+        add_subdirectory("${PROJECT_SOURCE_DIR}/tests/googletest-src"
+                        "${CMAKE_BINARY_DIR}/googletest-build")
+
+        if(CMAKE_VERSION VERSION_LESS 2.8.11)
+            include_directories("${gtest_SOURCE_DIR}/include"
+                                "${gmock_SOURCE_DIR}/include")
+        endif()
     endif()
 else()
-    message(STATUS "${PROJECT_NAME } is configured as a submodule. Skipping commond external depenencies!")
+    message(STATUS "${PROJECT_NAME} is configured as a submodule. Skipping commond external depenencies!")
 endif()
