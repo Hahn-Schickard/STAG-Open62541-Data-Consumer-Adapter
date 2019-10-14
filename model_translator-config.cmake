77e44f60a086d0b04de449c214f11dbb7b66b154
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2019-10-14 12:32:02 +0200

    removed manual config file

--- model_translator-config.cmake
+++
@@ -1,4 +0,0 @@
-get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include(${CMAKE_CURRENT_LIST_DIR}/lib/Model_Translator.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/modules/Data_Consumer_Notifier-0.0.0-Linux/data_consumer_notifier-config.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/modules/Information_Modeler-0.0.0-Linux/information_modeler-config.cmake)
