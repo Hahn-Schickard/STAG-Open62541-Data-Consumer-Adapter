2e244a9b454163f0540912138ab46263c72921dd
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2019-10-23 15:10:09 +0200

    syncing cpp project template v.0.8.14

--- cmake/compiler_settings.cmake
+++ cmake/compiler_settings.cmake
@@ -4,26 +4,31 @@ if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
 elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
     message(STATUS "Using GNU compiler")
 
+add_definitions(
+    -W
+    -Wunused-variable 
+    -Wunused-parameter 
+    -Wunused-function 
+    -Wunused 
+    -Wno-system-headers
+    -Wno-deprecated 
+    -Woverloaded-virtual 
+    -Wwrite-strings
+    -Wall        
+)
+
+if (NOT WIN32)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fprofile-arcs -ftest-coverage --coverage")
     set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -lgcov --coverage")
     
-    add_definitions(
-        -W
-        -Wunused-variable 
-        -Wunused-parameter 
-        -Wunused-function 
-        -Wunused 
-        -Wno-system-headers
-        -Wno-deprecated 
-        -Woverloaded-virtual 
-        -Wwrite-strings
-        -Wall
+    add_definitions(        
         -fprofile-arcs 
         -ftest-coverage
         -fdiagnostics-color=auto
         -lgcov
         --coverage
     )
+endif()
 
 elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
   # setup your MSVC toolchain here
