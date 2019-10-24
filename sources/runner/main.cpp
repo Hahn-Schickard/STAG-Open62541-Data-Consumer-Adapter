cd7084802fe14ba7c0e5f58729639f98e71e27a9
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2019-10-24 16:48:12 +0200

    added arg to main for server lifetime

--- sources/runner/main.cpp
+++ sources/runner/main.cpp
@@ -1,15 +1,42 @@
 #include "OpcuaAdapter.hpp"
 
+#include <iostream>
+#include <signal.h>
+
 using namespace std;
 
-#define SERVER_LIFETIME 60
+OpcuaAdapter *adapter;
 
-int main() {
+void stopServer() {
+  adapter->stop();
+  delete adapter;
+}
 
-  OpcuaAdapter *adapter = new OpcuaAdapter();
+static void stopHandler(int sig) {
+  cout << "Received stop signal!" << endl;
+  stopServer();
+}
+
+int main(int argc, char *argv[]) {
+  signal(SIGINT, stopHandler);
+  signal(SIGTERM, stopHandler);
+
+  adapter = new OpcuaAdapter();
   adapter->start();
 
-  sleep(SERVER_LIFETIME);
+  cout << "Started open62541 server!" << endl
+       << "Press Ctrl+C to stop the program!" << endl;
+
+  if (argc > 1) {
+    uint server_lifetime = atoi(argv[1]);
+    cout << "Open62541 server will automatically shut down in "
+         << server_lifetime << " seconds." << endl;
+    sleep(server_lifetime);
+    stopServer();
+  } else {
+    while (true)
+      ;
+  }
 
   exit(0);
 }
