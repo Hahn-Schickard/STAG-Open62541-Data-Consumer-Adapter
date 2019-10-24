672bb040e873b047a3acb38c050cd3e8197b4cf3
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2019-10-24 16:07:35 +0200

    simplified data consumer targets

--- sources/Server_Runner/Server_Runner.cpp
+++
@@ -1,9 +0,0 @@
-#include "Server_Runner.hpp"
-#include "Open62541_Server.hpp"
-
-using namespace std;
-using namespace Information_Model;
-
-bool start() { return startServer(); }
-bool stop() { return stopServer(); }
-void addDevice(shared_ptr<Device> device) { addDeviceNode(device); }
