3810acc442080a1bfeccd78538a7b2fca60a5ece
10 <dovydas.girdvainis@hahn-schickard.de>
2019-11-29 17:42:27 +0100

    Autoformated files with clang-format!
    Signed-off-by: 10 <dovydas.girdvainis@hahn-schickard.de>

--- includes/Opcua_Adapter/ConsumerNotifier.hpp
+++ includes/Opcua_Adapter/ConsumerNotifier.hpp
@@ -5,10 +5,10 @@
 #include "Listener.hpp"
 
 namespace OPCUA_Notifier {
-class ConsumerNotifier : public Model_Event_Handler::Listener {
-public:
-  virtual void handleEvent(Model_Event_Handler::NotifierEvent *event) = 0;
-};
-} // namespace OPCUA_Notifier
+  class ConsumerNotifier : public Model_Event_Handler::Listener {
+   public:
+    virtual void handleEvent(Model_Event_Handler::NotifierEvent* event) = 0;
+  };
+}   // namespace OPCUA_Notifier
 
-#endif //__CONSUMER_NOTIFIER_HPP
+#endif   //__CONSUMER_NOTIFIER_HPP
