57a341d580851316afd9ed851ddfc7e3e8ef86fb
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-09-24 11:52:18 +0200

    remove static logger from config serializer

--- sources/Serializer/Config_Serializer.cpp
+++ sources/Serializer/Config_Serializer.cpp
@@ -14,10 +14,6 @@ using namespace HaSLL;
 #define VERSION(MAJOR, MINOR, PATCH, LABEL)                                    \
   STRINGIFY(MAJOR) "." STRINGIFY(MINOR) "." STRINGIFY(PATCH) LABEL
 
-shared_ptr<Logger> serializer_logger =
-    LoggerRepository::getInstance().registerLoger(
-        "Open62541 Config Serializer");
-
 UA_UInt32Range initUInt32Range(UA_UInt32 min, UA_UInt32 max) {
   UA_UInt32Range range = {min, max};
   return range;
@@ -118,18 +114,13 @@ NLOHMANN_JSON_SERIALIZE_ENUM(UA_RuleHandling,
 namespace nlohmann {
 // ======================== UA_String ============================
 static void from_json(const json &j, UA_String &p) {
-  try {
-    string text = j.get<string>();
-    if (!text.empty()) {
-      p.length = strlen(text.c_str());
-      p.data = (UA_Byte *)malloc(p.length);
-      memcpy(p.data, text.c_str(), p.length);
-    } else {
-      p = UA_STRING_NULL;
-    }
-  } catch (exception &ex) {
-    serializer_logger->log(SeverityLevel::ERROR, "Received exception: {}",
-                           ex.what());
+  string text = j.get<string>();
+  if (!text.empty()) {
+    p.length = strlen(text.c_str());
+    p.data = (UA_Byte *)malloc(p.length);
+    memcpy(p.data, text.c_str(), p.length);
+  } else {
+    p = UA_STRING_NULL;
   }
 }
 
@@ -140,13 +131,8 @@ static void to_json(json &j, const UA_String &p) {
 
 // ======================== UA_LocalizedText ============================
 static void from_json(const json &j, UA_LocalizedText &p) {
-  try {
-    p.locale = j.at("locale").get<UA_String>();
-    p.text = j.at("text").get<UA_String>();
-  } catch (exception &ex) {
-    serializer_logger->log(SeverityLevel::ERROR, "Received exception: {}",
-                           ex.what());
-  }
+  p.locale = j.at("locale").get<UA_String>();
+  p.text = j.at("text").get<UA_String>();
 }
 
 static void to_json(json &j, const UA_LocalizedText &p) {
@@ -155,13 +141,8 @@ static void to_json(json &j, const UA_LocalizedText &p) {
 
 // ======================== UA_DurationRange ============================
 static void from_json(const json &j, UA_DurationRange &p) {
-  try {
-    p.min = j.at("min").get<UA_Duration>();
-    p.max = j.at("max").get<UA_Duration>();
-  } catch (exception &ex) {
-    serializer_logger->log(SeverityLevel::ERROR, "Received exception: {}",
-                           ex.what());
-  }
+  p.min = j.at("min").get<UA_Duration>();
+  p.max = j.at("max").get<UA_Duration>();
 }
 
 static void to_json(json &j, const UA_DurationRange &p) {
@@ -170,13 +151,8 @@ static void to_json(json &j, const UA_DurationRange &p) {
 
 // ======================== UA_UInt32Range ============================
 static void from_json(const json &j, UA_UInt32Range &p) {
-  try {
-    p.min = j.at("min").get<UA_UInt32>();
-    p.max = j.at("max").get<UA_UInt32>();
-  } catch (exception &ex) {
-    serializer_logger->log(SeverityLevel::ERROR, "Received exception: {}",
-                           ex.what());
-  }
+  p.min = j.at("min").get<UA_UInt32>();
+  p.max = j.at("max").get<UA_UInt32>();
 }
 
 static void to_json(json &j, const UA_UInt32Range &p) {
@@ -185,13 +161,8 @@ static void to_json(json &j, const UA_UInt32Range &p) {
 
 // ======================== SecureChannelsLimts ============================
 static void from_json(const json &j, UserCredentials &p) {
-  try {
-    p.username = j.at("username").get<UA_String>();
-    p.password = j.at("password").get<UA_String>();
-  } catch (exception &ex) {
-    serializer_logger->log(SeverityLevel::ERROR, "Received exception: {}",
-                           ex.what());
-  }
+  p.username = j.at("username").get<UA_String>();
+  p.password = j.at("password").get<UA_String>();
 }
 
 static void to_json(json &j, const UserCredentials &p) {
@@ -200,14 +171,9 @@ static void to_json(json &j, const UserCredentials &p) {
 
 // ======================== SecureChannelsLimts ============================
 static void from_json(const json &j, SecureChannelsLimts &p) {
-  try {
-    p.max_secure_channels = j.at("max_secure_channels").get<UA_UInt16>();
-    p.max_security_token_lifetime_ms =
-        j.at("max_security_token_lifetime_ms").get<UA_UInt32>();
-  } catch (exception &ex) {
-    serializer_logger->log(SeverityLevel::ERROR, "Received exception: {}",
-                           ex.what());
-  }
+  p.max_secure_channels = j.at("max_secure_channels").get<UA_UInt16>();
+  p.max_security_token_lifetime_ms =
+      j.at("max_security_token_lifetime_ms").get<UA_UInt32>();
 }
 
 static void to_json(json &j, const SecureChannelsLimts &p) {
@@ -218,13 +184,8 @@ static void to_json(json &j, const SecureChannelsLimts &p) {
 
 // ============================ SessionsLimits ===============================
 static void from_json(const json &j, SessionsLimits &p) {
-  try {
-    p.max_sessions = j.at("max_sessions").get<UA_UInt16>();
-    p.max_session_timeout_ms = j.at("max_session_timeout_ms").get<UA_Double>();
-  } catch (exception &ex) {
-    serializer_logger->log(SeverityLevel::ERROR, "Received exception: {}",
-                           ex.what());
-  }
+  p.max_sessions = j.at("max_sessions").get<UA_UInt16>();
+  p.max_session_timeout_ms = j.at("max_session_timeout_ms").get<UA_Double>();
 }
 
 static void to_json(json &j, const SessionsLimits &p) {
@@ -234,25 +195,19 @@ static void to_json(json &j, const SessionsLimits &p) {
 
 // ======================== OperationalLimits ===========================
 static void from_json(const json &j, OperationalLimits &p) {
-  try {
-    p.max_nodes_per_read = j.at("max_nodes_per_read").get<UA_UInt32>();
-    p.max_nodes_per_write = j.at("max_nodes_per_write").get<UA_UInt32>();
-    p.max_nodes_per_method_call =
-        j.at("max_nodes_per_method_call").get<UA_UInt32>();
-    p.max_nodes_per_browse = j.at("max_nodes_per_browse").get<UA_UInt32>();
-    p.max_nodes_per_register_nodes =
-        j.at("max_nodes_per_register_nodes").get<UA_UInt32>();
-    p.max_nodes_per_translate_browse_paths_to_nodeids =
-        j.at("max_nodes_per_translate_browse_paths_to_nodeids")
-            .get<UA_UInt32>();
-    p.max_nodes_per_node_management =
-        j.at("max_nodes_per_node_management").get<UA_UInt32>();
-    p.max_monitored_items_per_call =
-        j.at("max_monitored_items_per_call").get<UA_UInt32>();
-  } catch (exception &ex) {
-    serializer_logger->log(SeverityLevel::ERROR, "Received exception: {}",
-                           ex.what());
-  }
+  p.max_nodes_per_read = j.at("max_nodes_per_read").get<UA_UInt32>();
+  p.max_nodes_per_write = j.at("max_nodes_per_write").get<UA_UInt32>();
+  p.max_nodes_per_method_call =
+      j.at("max_nodes_per_method_call").get<UA_UInt32>();
+  p.max_nodes_per_browse = j.at("max_nodes_per_browse").get<UA_UInt32>();
+  p.max_nodes_per_register_nodes =
+      j.at("max_nodes_per_register_nodes").get<UA_UInt32>();
+  p.max_nodes_per_translate_browse_paths_to_nodeids =
+      j.at("max_nodes_per_translate_browse_paths_to_nodeids").get<UA_UInt32>();
+  p.max_nodes_per_node_management =
+      j.at("max_nodes_per_node_management").get<UA_UInt32>();
+  p.max_monitored_items_per_call =
+      j.at("max_monitored_items_per_call").get<UA_UInt32>();
 }
 
 static void to_json(json &j, const OperationalLimits &p) {
@@ -269,27 +224,22 @@ static void to_json(json &j, const OperationalLimits &p) {
 
 // ======================== SubscriptionsLimtis ===========================
 static void from_json(const json &j, SubscriptionsLimtis &p) {
-  try {
-    p.max_subscriptions = j.at("max_subscriptions").get<UA_UInt32>();
-    p.max_subscriptions_per_session =
-        j.at("max_subscriptions_per_session").get<UA_UInt32>();
-    p.publishing_interval_limits_ms =
-        j.at("publishing_interval_limits_ms").get<UA_DurationRange>();
-    p.life_time_count_limits =
-        j.at("life_time_count_limits").get<UA_UInt32Range>();
-    p.keep_alive_count_limits =
-        j.at("keep_alive_count_limits").get<UA_UInt32Range>();
-    p.max_notifications_per_publish =
-        j.at("max_notifications_per_publish").get<UA_UInt32>();
-    p.enable_retransmission_queue =
-        j.at("enable_retransmission_queue").get<UA_Boolean>();
-    p.max_retransmission_queue_size =
-        j.at("max_retransmission_queue_size").get<UA_UInt32>();
-    p.max_events_per_node = j.at("max_events_per_node").get<UA_UInt32>();
-  } catch (exception &ex) {
-    serializer_logger->log(SeverityLevel::ERROR, "Received exception: {}",
-                           ex.what());
-  }
+  p.max_subscriptions = j.at("max_subscriptions").get<UA_UInt32>();
+  p.max_subscriptions_per_session =
+      j.at("max_subscriptions_per_session").get<UA_UInt32>();
+  p.publishing_interval_limits_ms =
+      j.at("publishing_interval_limits_ms").get<UA_DurationRange>();
+  p.life_time_count_limits =
+      j.at("life_time_count_limits").get<UA_UInt32Range>();
+  p.keep_alive_count_limits =
+      j.at("keep_alive_count_limits").get<UA_UInt32Range>();
+  p.max_notifications_per_publish =
+      j.at("max_notifications_per_publish").get<UA_UInt32>();
+  p.enable_retransmission_queue =
+      j.at("enable_retransmission_queue").get<UA_Boolean>();
+  p.max_retransmission_queue_size =
+      j.at("max_retransmission_queue_size").get<UA_UInt32>();
+  p.max_events_per_node = j.at("max_events_per_node").get<UA_UInt32>();
 }
 
 static void to_json(json &j, const SubscriptionsLimtis &p) {
@@ -306,17 +256,12 @@ static void to_json(json &j, const SubscriptionsLimtis &p) {
 
 // ======================== MonitoredItemsLimits ===========================
 static void from_json(const json &j, MonitoredItemsLimits &p) {
-  try {
-    p.max_monitored_items = j.at("max_monitored_items").get<UA_UInt32>();
-    p.max_monitored_items_per_subscription =
-        j.at("max_monitored_items_per_subscription").get<UA_UInt32>();
-    p.sampling_interval_limits_ms =
-        j.at("sampling_interval_limits_ms").get<UA_DurationRange>();
-    p.queue_size_limits = j.at("queue_size_limits").get<UA_UInt32Range>();
-  } catch (exception &ex) {
-    serializer_logger->log(SeverityLevel::ERROR, "Received exception: {}",
-                           ex.what());
-  }
+  p.max_monitored_items = j.at("max_monitored_items").get<UA_UInt32>();
+  p.max_monitored_items_per_subscription =
+      j.at("max_monitored_items_per_subscription").get<UA_UInt32>();
+  p.sampling_interval_limits_ms =
+      j.at("sampling_interval_limits_ms").get<UA_DurationRange>();
+  p.queue_size_limits = j.at("queue_size_limits").get<UA_UInt32Range>();
 }
 
 static void to_json(json &j, const MonitoredItemsLimits &p) {
@@ -329,14 +274,9 @@ static void to_json(json &j, const MonitoredItemsLimits &p) {
 
 // ======================== UA_ConnectionConfig ===========================
 static void from_json(const json &j, UA_ConnectionConfig &p) {
-  try {
-    p.protocolVersion = j.at("protocolVersion").get<UA_UInt32>();
-    p.recvBufferSize = j.at("recvBufferSize").get<UA_UInt32>();
-    p.sendBufferSize = j.at("sendBufferSize").get<UA_UInt32>();
-  } catch (exception &ex) {
-    serializer_logger->log(SeverityLevel::ERROR, "Received exception: {}",
-                           ex.what());
-  }
+  p.protocolVersion = j.at("protocolVersion").get<UA_UInt32>();
+  p.recvBufferSize = j.at("recvBufferSize").get<UA_UInt32>();
+  p.sendBufferSize = j.at("sendBufferSize").get<UA_UInt32>();
 }
 
 static void to_json(json &j, const UA_ConnectionConfig &p) {
@@ -347,17 +287,12 @@ static void to_json(json &j, const UA_ConnectionConfig &p) {
 
 // ======================== UA_BuildInfo ===========================
 static void from_json(const json &j, UA_BuildInfo &p) {
-  try {
-    p.productUri = j.at("productUri").get<UA_String>();
-    p.manufacturerName = j.at("manufacturerName").get<UA_String>();
-    p.productName = j.at("productName").get<UA_String>();
-    p.softwareVersion = j.at("softwareVersion").get<UA_String>();
-    p.buildNumber = j.at("buildNumber").get<UA_String>();
-    p.buildDate = j.at("buildDate").get<UA_DateTime>();
-  } catch (exception &ex) {
-    serializer_logger->log(SeverityLevel::ERROR, "Received exception: {}",
-                           ex.what());
-  }
+  p.productUri = j.at("productUri").get<UA_String>();
+  p.manufacturerName = j.at("manufacturerName").get<UA_String>();
+  p.productName = j.at("productName").get<UA_String>();
+  p.softwareVersion = j.at("softwareVersion").get<UA_String>();
+  p.buildNumber = j.at("buildNumber").get<UA_String>();
+  p.buildDate = j.at("buildDate").get<UA_DateTime>();
 }
 
 static void to_json(json &j, const UA_BuildInfo &p) {
@@ -369,19 +304,14 @@ static void to_json(json &j, const UA_BuildInfo &p) {
 
 // ====================== UA_ApplicationDescription =======================
 static void from_json(const json &j, UA_ApplicationDescription &p) {
-  try {
-    p.applicationUri = j.at("applicationUri").get<UA_String>();
-    p.productUri = j.at("productUri").get<UA_String>();
-    p.applicationName = j.at("applicationName").get<UA_LocalizedText>();
-    p.applicationType = j.at("applicationType").get<UA_ApplicationType>();
-    p.gatewayServerUri = j.at("gatewayServerUri").get<UA_String>();
-    p.discoveryProfileUri = j.at("discoveryProfileUri").get<UA_String>();
-    p.discoveryUrlsSize = 0; // @TODO: handle discovery urls
-    p.discoveryUrls = nullptr;
-  } catch (exception &ex) {
-    serializer_logger->log(SeverityLevel::ERROR, "Received exception: {}",
-                           ex.what());
-  }
+  p.applicationUri = j.at("applicationUri").get<UA_String>();
+  p.productUri = j.at("productUri").get<UA_String>();
+  p.applicationName = j.at("applicationName").get<UA_LocalizedText>();
+  p.applicationType = j.at("applicationType").get<UA_ApplicationType>();
+  p.gatewayServerUri = j.at("gatewayServerUri").get<UA_String>();
+  p.discoveryProfileUri = j.at("discoveryProfileUri").get<UA_String>();
+  p.discoveryUrlsSize = 0; // @TODO: handle discovery urls
+  p.discoveryUrls = nullptr;
 }
 
 static void to_json(json &j, const UA_ApplicationDescription &p) {
@@ -395,13 +325,8 @@ static void to_json(json &j, const UA_ApplicationDescription &p) {
 
 // ==================== UA_MdnsDiscoveryConfiguration =====================
 static void from_json(const json &j, UA_MdnsDiscoveryConfiguration &p) {
-  try {
-    p.mdnsServerName = j.at("mdnsServerName").get<UA_String>();
-    //@TODO: handle server capabilities list
-  } catch (exception &ex) {
-    serializer_logger->log(SeverityLevel::ERROR, "Received exception: {}",
-                           ex.what());
-  }
+  p.mdnsServerName = j.at("mdnsServerName").get<UA_String>();
+  //@TODO: handle server capabilities list
 }
 
 static void to_json(json &j, const UA_MdnsDiscoveryConfiguration &p) {
@@ -410,13 +335,8 @@ static void to_json(json &j, const UA_MdnsDiscoveryConfiguration &p) {
 
 // ====================== UA_ServerConfig_Discovery =======================
 static void from_json(const json &j, UA_ServerConfig_Discovery &p) {
-  try {
-    p.cleanupTimeout = j.at("cleanupTimeout").get<UA_UInt32>();
-    p.mdnsEnable = j.at("mdnsEnable").get<bool>();
-  } catch (exception &ex) {
-    serializer_logger->log(SeverityLevel::ERROR, "Received exception: {}",
-                           ex.what());
-  }
+  p.cleanupTimeout = j.at("cleanupTimeout").get<UA_UInt32>();
+  p.mdnsEnable = j.at("mdnsEnable").get<bool>();
 }
 
 static void to_json(json &j, const UA_ServerConfig_Discovery &p) {
@@ -425,36 +345,29 @@ static void to_json(json &j, const UA_ServerConfig_Discovery &p) {
 
 // ======================== Config ===========================
 static void from_json(const json &j, Config &p) {
-  try {
-    p.allow_annonymous_access =
-        j.at("allow_annonymous_access").get<UA_Boolean>();
-    p.access_credentials = j.at("access_credentials").get<UserCredentials>();
-    p.thread_count = j.at("thread_count").get<UA_UInt16>();
-    p.port_nubmer = j.at("port_nubmer").get<UA_UInt16>();
-    p.networking = j.at("networking").get<UA_ConnectionConfig>();
-    p.security_policy = j.at("security_policy").get<SecurityPolicy>();
-    p.build_info = j.at("build_info").get<UA_BuildInfo>();
-    p.app_info = j.at("app_info").get<UA_ApplicationDescription>();
-    p.server_certificate = j.at("server_certificate").get<UA_ByteString>();
-    p.shutdown_delay_ms = j.at("shutdown_delay_ms").get<UA_Double>();
-    p.rules_handling = j.at("rules_handling").get<UA_RuleHandling>();
-    p.secure_channels_limits =
-        j.at("secure_channels_limits").get<SecureChannelsLimts>();
-    p.session_limits = j.at("session_limits").get<SessionsLimits>();
-    p.operation_limits = j.at("operation_limits").get<OperationalLimits>();
-    p.max_references_per_node =
-        j.at("max_references_per_node").get<UA_UInt32>();
-    p.subscription_limits =
-        j.at("subscription_limits").get<SubscriptionsLimtis>();
-    p.monitored_items_limits =
-        j.at("monitored_items_limits").get<MonitoredItemsLimits>();
-    p.max_publish_req_per_session =
-        j.at("max_publish_req_per_session").get<UA_UInt32>();
-    p.discovery = j.at("discovery").get<UA_ServerConfig_Discovery>();
-  } catch (exception &ex) {
-    serializer_logger->log(SeverityLevel::ERROR, "Received exception: {}",
-                           ex.what());
-  }
+  p.allow_annonymous_access = j.at("allow_annonymous_access").get<UA_Boolean>();
+  p.access_credentials = j.at("access_credentials").get<UserCredentials>();
+  p.thread_count = j.at("thread_count").get<UA_UInt16>();
+  p.port_nubmer = j.at("port_nubmer").get<UA_UInt16>();
+  p.networking = j.at("networking").get<UA_ConnectionConfig>();
+  p.security_policy = j.at("security_policy").get<SecurityPolicy>();
+  p.build_info = j.at("build_info").get<UA_BuildInfo>();
+  p.app_info = j.at("app_info").get<UA_ApplicationDescription>();
+  p.server_certificate = j.at("server_certificate").get<UA_ByteString>();
+  p.shutdown_delay_ms = j.at("shutdown_delay_ms").get<UA_Double>();
+  p.rules_handling = j.at("rules_handling").get<UA_RuleHandling>();
+  p.secure_channels_limits =
+      j.at("secure_channels_limits").get<SecureChannelsLimts>();
+  p.session_limits = j.at("session_limits").get<SessionsLimits>();
+  p.operation_limits = j.at("operation_limits").get<OperationalLimits>();
+  p.max_references_per_node = j.at("max_references_per_node").get<UA_UInt32>();
+  p.subscription_limits =
+      j.at("subscription_limits").get<SubscriptionsLimtis>();
+  p.monitored_items_limits =
+      j.at("monitored_items_limits").get<MonitoredItemsLimits>();
+  p.max_publish_req_per_session =
+      j.at("max_publish_req_per_session").get<UA_UInt32>();
+  p.discovery = j.at("discovery").get<UA_ServerConfig_Discovery>();
 }
 
 static void to_json(json &j, const Config &p) {
@@ -487,10 +400,6 @@ const Config open62541::deserializeConfig(const string &file_path) {
     nlohmann::json j;
     input_file_stream >> j;
     config = j.get<Config>();
-  } else {
-    serializer_logger->log(SeverityLevel::ERROR,
-                           "Failled to open file striem for file: {}",
-                           file_path);
   }
   return move(config);
 }
@@ -501,10 +410,6 @@ void open62541::serializeConfig(const string &file_path, const Config &config) {
     nlohmann::json j = config;
 
     output_file_stream << j << endl;
-  } else {
-    serializer_logger->log(SeverityLevel::ERROR,
-                           "Failled to open file striem for file: {}",
-                           file_path);
   }
 }
 
