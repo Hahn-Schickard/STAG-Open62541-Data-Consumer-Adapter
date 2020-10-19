d7add7bd43bc1c967ae7e31646c4de3742d96e1c
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-10-19 18:28:07 +0200

    fix conan build_type setting

--- cmake/conan_install.cmake
+++ cmake/conan_install.cmake
@@ -36,34 +36,34 @@ macro(execute_conan_install)
       endif()
     endif()
 
-    execute_process(COMMAND ${conan_command} install ${CONANFILE}
+    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+      message(STATUS "Debug configuration detected! Configuring dependencies with build_type=Debug")
+      set(CONAN_ARGS -s build_type=Debug)
+    endif()
+
+    set(ARGS "${CONAN_ARGS}")
+    message(STATUS "Executing ${conan_command} install ${CONANFILE} ${ARGS}")
+    execute_process(COMMAND ${conan_command} install ${CONANFILE} ${ARGS}
                     RESULT_VARIABLE return_code
-                    OUTPUT_VARIABLE conan_output
                     ERROR_VARIABLE conan_error
                     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
     )
-
-    if(NOT SILENT)
-      message("${conan_output}")
-    endif()
     
     if(NOT "${return_code}" STREQUAL "0")
-      message(WARNING "Conan failed to install failed!")
+      message(WARNING "Conan failed to install dependencies!")
       message("${conan_error}")
       message(WARNING "Trying to build sources locally!")
-      execute_process(COMMAND ${conan_command} install ${CONANFILE} --build
+      set(ARGS --build ${CONAN_ARGS})
+      message(STATUS "Executing ${conan_command} install ${CONANFILE} ${ARGS}")
+      execute_process(COMMAND ${conan_command} install ${CONANFILE} ${ARGS}
                       RESULT_VARIABLE return_code
-                      OUTPUT_VARIABLE conan_output
                       ERROR_VARIABLE conan_error
                       WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
       )
 
-      if(NOT SILENT)
-        message("${conan_output}")
-      endif()
-
       if(NOT "${return_code}" STREQUAL "0")
         message(FATAL_ERROR "${conan_error}")
+        message(STATUS "Try running: ${conan_command} install ${CONANFILE} ${ARGS}")
       endif()
     endif()
     message(STATUS "Using generated conan paths from: ${CMAKE_BINARY_DIR}/conan_paths.cmake")
