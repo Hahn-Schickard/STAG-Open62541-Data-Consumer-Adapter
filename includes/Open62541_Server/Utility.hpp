b18552424068b36e08aff11f6533524bb99ac3a0
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-10-29 17:14:39 +0100

    add macro for unused function/method parameters

--- includes/Open62541_Server/Utility.hpp
+++ includes/Open62541_Server/Utility.hpp
@@ -7,6 +7,12 @@
 #include <open62541/types.h>
 #include <string>
 
+#ifdef __GNUC__
+#define UNUSED(x) x##_UNUSED __attribute__((__unused__))
+#else
+#define UNUSED(x) x##_UNUSED
+#endif
+
 namespace open62541 {
 
 UA_NodeId toNodeId(Information_Model::DataType type);
