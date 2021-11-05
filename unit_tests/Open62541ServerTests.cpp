ab9618c63e58e71b2bc5934914faa215035a8206
Mark Weyer <Mark.Weyer@hahn-schickard.de>
2021-11-05 10:40:41 +0100

    Move logger initialization to testRunner.cpp

--- unit_tests/Open62541ServerTests.cpp
+++ unit_tests/Open62541ServerTests.cpp
@@ -1,21 +1,12 @@
 #include "gtest/gtest.h"
 
-#include "LoggerRepository.hpp"
 #include "Open62541Server.hpp"
 
 namespace Open62541ServerTests {
 
 using namespace open62541;
 
-struct Open62541ServerTests : public ::testing::Test {
-  Open62541ServerTests() {
-    auto config = HaSLL::Configuration(
-      "./log", "logfile.log", "[%Y-%m-%d-%H:%M:%S:%F %z][%n]%^[%l]: %v%$",
-      HaSLL::SeverityLevel::TRACE, false, 8192, 2, 25, 100, 1);
-    HaSLL::LoggerRepository::initialise(config);
-  }
-};
-
+struct Open62541ServerTests : public ::testing::Test {};
 
 TEST_F(Open62541ServerTests, defaultConstructorWorks) {
   Open62541Server server;
