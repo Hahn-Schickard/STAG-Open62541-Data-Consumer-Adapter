045db95fcf8cad6c2f8f94f680a2f92ef1aa0a98
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2022-10-27 16:18:58 +0200

    refactor extern into inline for shared test resources

--- unit_tests/SharedTestResources.hpp
+++ unit_tests/SharedTestResources.hpp
@@ -1,7 +1,15 @@
+#ifndef __OPEN62541_SHARED_TEST_RESORUCES_HPP
+#define __OPEN62541_SHARED_TEST_RESORUCES_HPP
+
 #include "NodeBuilder.hpp"
 #include "Open62541Server.hpp"
 
 #include <memory>
 
-extern std::shared_ptr<open62541::Open62541Server> shared_server;
-extern std::shared_ptr<open62541::NodeBuilder> shared_node_builder;
+using Open62541ServerPtr = std::shared_ptr<open62541::Open62541Server>;
+using NodeBuilderPtr = std::shared_ptr<open62541::NodeBuilder>;
+
+inline Open62541ServerPtr shared_server;
+inline NodeBuilderPtr shared_node_builder;
+
+#endif //__OPEN62541_SHARED_TEST_RESORUCES_HPP
--- unit_tests/testRunner.cpp
+++ unit_tests/testRunner.cpp
@@ -1,35 +1,26 @@
+#include "SharedTestResources.hpp"
+
 #include "HaSLL/LoggerManager.hpp"
 #include "HaSLL/SPD_LoggerRepository.hpp"
-
-#include "SharedTestResources.hpp"
 #include "gtest/gtest.h"
 
 using namespace std;
 using namespace HaSLL;
-
-shared_ptr<open62541::Open62541Server> shared_server;
-std::shared_ptr<open62541::NodeBuilder> shared_node_builder;
+using namespace open62541;
 
 int main(int argc, char** argv) {
   auto repo = make_shared<SPD_LoggerRepository>();
   LoggerManager::initialise(repo);
 
-  auto logger = LoggerManager::registerLogger("Test Runner");
+  shared_server = make_shared<Open62541Server>();
+  shared_node_builder = make_shared<NodeBuilder>(shared_server);
 
-  logger->info("Registering shared test results");
-  shared_server = std::make_shared<open62541::Open62541Server>();
-  shared_node_builder = std::make_shared<open62541::NodeBuilder>(shared_server);
-
-  logger->info("Initializing Google Tests");
   testing::InitGoogleTest(&argc, argv);
-
-  logger->info("Running tests");
   auto status = RUN_ALL_TESTS();
 
-  logger->info("Tests finished, cleaning up shared resources");
   shared_server.reset();
   shared_node_builder.reset();
-
   LoggerManager::terminate();
+
   return status;
 }
