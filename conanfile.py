6c0c903e57d1b29237581301f841c8ab91a4ede9
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2022-11-03 10:48:58 +0100

    bump HaSLL to v0.3.2

--- conanfile.py
+++ conanfile.py
@@ -12,7 +12,7 @@ class PackageConan(ConanFile):
     requires = [
         "nlohmann_json/3.11.1",
         "open62541/1.1.3",
-        "HaSLL/0.3.1@hahn-schickard/stable",
+        "HaSLL/0.3.2@hahn-schickard/stable",
         "Variant_Visitor/0.1.0@hahn-schickard/stable",
         "Data_Consumer_Adapter_Interface/0.1.9@hahn-schickard/stable"
     ]
