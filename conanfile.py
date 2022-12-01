72481d686526ad250c46365723b2475048270a2c
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2022-12-01 17:04:19 +0100

    bump open62541 to v1.31

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,7 @@ class PackageConan(ConanFile):
     build_requires = 'gtest/[~1.11]'
     requires = [
         "nlohmann_json/3.11.1",
-        "open62541/1.2.4",
+        "open62541/1.3.1",
         "HaSLL/[~0.3]@hahn-schickard/stable",
         "Variant_Visitor/[~0.1]@hahn-schickard/stable",
         "Data_Consumer_Adapter_Interface/[~0.1]@hahn-schickard/stable"
