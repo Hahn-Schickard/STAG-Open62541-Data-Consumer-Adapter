9094606d410610239f5a57034c8674865d59047f
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2023-07-04 18:04:31 +0200

    refactor NodeCallbackHandler::readNodeValue() to use toUAVariant()

--- sources/Open62541_Server/NodeCallbackHandler.cpp
+++ sources/Open62541_Server/NodeCallbackHandler.cpp
@@ -104,106 +104,24 @@ UA_StatusCode NodeCallbackHandler::readNodeValue( // clang-format off
     auto callback_wrapper = it->second;
     try {
       if (callback_wrapper->readable_) {
-        auto variant_value = callback_wrapper->readable_();
-        match(
-            variant_value,
-            [&](bool boolean_value) {
-              if (callback_wrapper->data_type_ == DataType::BOOLEAN) {
-                UA_Variant_setScalarCopy(
-                    &value->value, &boolean_value, &UA_TYPES[UA_TYPES_BOOLEAN]);
-              } else {
-                throw runtime_error("Tried to read a Boolean data type "
-                                    "when node data type is: " +
-                    toString(callback_wrapper->data_type_));
-              }
-            },
-            [&](uintmax_t integer_value) {
-              if (callback_wrapper->data_type_ == DataType::UNSIGNED_INTEGER) {
-                UA_Variant_setScalarCopy(
-                    &value->value, &integer_value, &UA_TYPES[UA_TYPES_uintmax]);
-              } else {
-                throw runtime_error("Tried to read an Integer data type "
-                                    "when node data type is:" +
-                    toString(callback_wrapper->data_type_));
-              }
-            },
-            [&](intmax_t long_value) {
-              if (callback_wrapper->data_type_ == DataType::INTEGER) {
-                UA_Variant_setScalarCopy(
-                    &value->value, &long_value, &UA_TYPES[UA_TYPES_intmax]);
-              } else {
-                throw runtime_error("Tried to read a Long data type "
-                                    "when node data type is: " +
-                    toString(callback_wrapper->data_type_));
-              }
-            },
-            [&](double double_value) {
-              if (callback_wrapper->data_type_ == DataType::DOUBLE) {
-                UA_Variant_setScalarCopy(
-                    &value->value, &double_value, &UA_TYPES[UA_TYPES_DOUBLE]);
-              } else {
-                throw runtime_error("Tried to read a Double data type "
-                                    "when node data type is:" +
-                    toString(callback_wrapper->data_type_));
-              }
-            },
-            [&](DateTime time_value) {
-              if (callback_wrapper->data_type_ == DataType::TIME) {
-                UA_DateTime date_time =
-                    UA_DateTime_fromUnixTime(time_value.getValue());
-                UA_Variant_setScalarCopy(
-                    &value->value, &date_time, &UA_TYPES[UA_TYPES_DATETIME]);
-              } else {
-                throw runtime_error("Tried to read a Time data type "
-                                    "when node data type is: " +
-                    toString(callback_wrapper->data_type_));
-              }
-            },
-            [&](vector<uint8_t> opaque_value) {
-              if (callback_wrapper->data_type_ == DataType::OPAQUE) {
-                UA_ByteString byte_string;
-                byte_string.length = opaque_value.size();
-                byte_string.data = (UA_Byte*)malloc(byte_string.length);
-                memcpy(
-                    byte_string.data, opaque_value.data(), byte_string.length);
-                UA_Variant_setScalarCopy(&value->value, &byte_string,
-                    &UA_TYPES[UA_TYPES_BYTESTRING]);
-                UA_String_clear(&byte_string);
-              } else {
-                throw runtime_error("Tried to read an Opaque data type "
-                                    "when node data type is: " +
-                    toString(callback_wrapper->data_type_));
-              }
-            },
-            [&](const string& string_value) {
-              if (callback_wrapper->data_type_ == DataType::STRING) {
-                UA_String open62541_string;
-                open62541_string.length = strlen(string_value.c_str());
-                open62541_string.data =
-                    (UA_Byte*)malloc(open62541_string.length);
-                memcpy(open62541_string.data, string_value.c_str(),
-                    open62541_string.length);
-                UA_Variant_setScalarCopy(&value->value, &open62541_string,
-                    &UA_TYPES[UA_TYPES_STRING]);
-                UA_String_clear(&open62541_string);
-              } else {
-                throw runtime_error("Tried to read a String data type "
-                                    "when node data type is: " +
-                    toString(callback_wrapper->data_type_));
-              }
-            });
-        value->hasValue = true;
-        status = UA_STATUSCODE_GOOD;
+        auto data_variant = callback_wrapper->readable_();
+        if (toDataType(data_variant) == callback_wrapper->data_type_) {
+          value->value = toUAVariant(data_variant);
+          value->hasValue = true;
+          status = UA_STATUSCODE_GOOD;
+        } else {
+          string error_msg = "Expected to receive " +
+              toString(callback_wrapper->data_type_) +
+              " data type, but received " + toString(toDataType(data_variant)) +
+              " instead while reading Node " + toString(node_id) + " value";
+          UA_LOG_ERROR(logger_, UA_LOGCATEGORY_SERVER, error_msg.c_str());
+          status = UA_STATUSCODE_BADTYPEMISMATCH;
+        }
       } else {
         string error_msg = "Node " + toString(node_id) +
             " does not have any registered read callback!";
         UA_LOG_ERROR(logger_, UA_LOGCATEGORY_SERVER, error_msg.c_str());
       }
-    } catch (const runtime_error& error) {
-      string error_msg = "Type missmatch error occurred while reading Node " +
-          toString(node_id) + " Error message: " + error.what();
-      UA_LOG_ERROR(logger_, UA_LOGCATEGORY_SERVER, error_msg.c_str());
-      status = UA_STATUSCODE_BADTYPEMISMATCH;
     } catch (const exception& exp) {
       string error_msg = "An exception occurred, while reading Node " +
           toString(node_id) + " exception: " + exp.what();
@@ -214,6 +132,7 @@ UA_StatusCode NodeCallbackHandler::readNodeValue( // clang-format off
     string error_msg = "Node " + toString(node_id) +
         " does not have any registered read callbacks!";
     UA_LOG_ERROR(logger_, UA_LOGCATEGORY_SERVER, error_msg.c_str());
+    status = UA_STATUSCODE_BADNOTFOUND;
   }
   return status;
 }
