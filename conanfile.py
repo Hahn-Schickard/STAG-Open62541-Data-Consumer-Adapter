0653dee392ac8148cac7b484473708065aa9486e
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2023-07-26 14:58:11 +0200

    update project template to v0.2.3

--- conanfile.py
+++ conanfile.py
@@ -1,55 +1,42 @@
 from conan import ConanFile
-from conan.tools.files import load, collect_libs
-from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain, CMakeDeps
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import load, copy, collect_libs
+from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
 import re
 import os
 
 
+def to_camel_case(input: str):
+    words = input.replace("_", " ").split()
+    return '_'.join(word.capitalize() for word in words)
+
+
 class PackageConan(ConanFile):
+    # @+ START USER META CONFIG
     license = "Apache 2.0"
+    description = "OPC UA Server Technology Adapter implementation with open62541"
     topics = ('stag', 'dca', 'open62541')
-    build_requires = 'gtest/[~1.11]'
-    requires = [
-        "nlohmann_json/3.11.1",
-        "open62541/1.3.4",
-        "date/3.0.1",
-        "HaSLL/[~0.3]@hahn-schickard/stable",
-        "HSCUL/[~0.3]@hahn-schickard/stable",
-        "Variant_Visitor/[~0.1]@hahn-schickard/stable",
-        "Data_Consumer_Adapter_Interface/[~0.1]@hahn-schickard/stable"
-    ]
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False],
                "fPIC": [True, False],
                "historization": [True, False]}
-    default_options = {"open62541/*:cpp_compatible": True,
-                       "open62541/*:multithreading": "Threadsafe",
-                       "shared": True,
+    default_options = {"shared": True,
                        "fPIC": True,
                        "historization": False}
     default_user = "Hahn-Schickard"
+    # @- END USER META CONFIG
     exports_sources = [
         "cmake*",
-        'config*',
         "includes*",
         "sources*",
-        "unit_tests*",
         "CMakeLists.txt",
         "conanfile.py",
-        'README.md',
-        "LICENSE",
-        "NOTICE",
-        "AUTHORS"
+        # @+ START USER EXPORTS
+        'config*',
+        # @- END USER EXPORTS
     ]
+    generators = "CMakeDeps"
     short_paths = True
-
-    def layout(self):
-        cmake_layout(self)
-
-    def requirements(self):
-        if self.options.historization:
-            self.requires("OODD/[~0.1]@hahn-schickard/stable")
-            self.options["open62541"].historize = True
 
     @property
     def cwd(self):
@@ -58,7 +45,35 @@ class PackageConan(ConanFile):
     def set_name(self):
         content = load(self, path=os.path.join(self.cwd, 'CMakeLists.txt'))
         name = re.search('set\(THIS (.*)\)', content).group(1)
-        self.name = name.strip()
+        self.name = name.strip().lower()
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, "17")
+
+    def requirements(self):
+        # @+ START USER REQUIREMENTS
+        self.requires("nlohmann_json/3.11.1")
+        self.requires("open62541/1.3.4", headers=True, libs=True,
+                      transitive_headers=True, transitive_libs=True)
+        self.requires("data_consumer_adapter_interface/[~0.2]@hahn-schickard/stable",
+                      headers=True, libs=True, transitive_headers=True, transitive_libs=True)
+        if self.options.historization:
+            self.requires("oodd/[~0.1]@hahn-schickard/stable",
+                          headers=True, libs=True, transitive_headers=True, transitive_libs=True)
+        self.test_requires("gtest/[~1.11]")
+        # @- END USER REQUIREMENTS
+
+    def configure(self):
+        # @+ START USER REQUIREMENTS OPTION CONFIGURATION
+        self.options["open62541"].cpp_compatible = True
+        self.options["open62541"].multithreading = "Threadsafe"
+        if self.options.historization:
+            self.options["open62541"].historize = True
+        # @- END USER REQUIREMENTS OPTION CONFIGURATION
+
+    def layout(self):
+        cmake_layout(self)
 
     def config_options(self):
         if self.settings.os == 'Windows':
@@ -68,11 +83,9 @@ class PackageConan(ConanFile):
         tc = CMakeToolchain(self)
         tc.variables['STATIC_CODE_ANALYSIS'] = False
         tc.variables['RUN_TESTS'] = False
-        tc.variables['USE_CONAN'] = True
+        tc.variables['CMAKE_CONAN'] = False
         tc.variables['HISTORIZATION'] = self.options.historization
         tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0077"] = "NEW"
-        tc.generate()
-        tc = CMakeDeps(self)
         tc.generate()
 
     def build(self):
@@ -83,11 +96,16 @@ class PackageConan(ConanFile):
     def package(self):
         cmake = CMake(self)
         cmake.install()
-        self.copy(pattern='LICENSE', dst='licenses', src=self.cwd)
-        self.copy(pattern='NOTICE', dst='licenses', src=self.cwd)
-        self.copy(pattern='AUTHORS', dst='licenses', src=self.cwd)
+        copy(self, pattern='LICENSE', dst='licenses', src=self.cwd)
+        copy(self, pattern='NOTICE', dst='licenses', src=self.cwd)
+        copy(self, pattern='AUTHORS', dst='licenses', src=self.cwd)
 
     def package_info(self):
         self.cpp_info.libs = collect_libs(self)
-        self.output.info('Collected libs: \n{}'.format(
-            '\n'.join(self.cpp_info.libs)))
+        self.cpp_info.set_property("cmake_find_mode", "both")
+        # @+ START USER DEFINES
+        self.cpp_info.set_property("cmake_file_name", to_camel_case(self.name))
+        cmake_target_name = to_camel_case(
+            self.name) + "::" + to_camel_case(self.name)
+        self.cpp_info.set_property("cmake_target_name", cmake_target_name)
+        # @- END USER DEFINES
