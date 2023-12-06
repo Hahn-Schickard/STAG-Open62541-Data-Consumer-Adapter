397ac617d73a57b530191e2efa449a66001da6bb
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2023-12-06 15:15:24 +0100

    remove Open62541ServerTests namespace

--- unit_tests/Device/Open62541ServerTests.cpp
+++ unit_tests/Device/Open62541ServerTests.cpp
@@ -2,8 +2,6 @@
 
 #include "Open62541Server.hpp"
 
-namespace Open62541ServerTests {
-
 using namespace open62541;
 
 struct Open62541ServerTests : public ::testing::Test {};
@@ -18,5 +16,3 @@ TEST_F(Open62541ServerTests, defaultConfigurationValid) {
   auto config = std::make_unique<Configuration>();
   Open62541Server server(std::move(config));
 }
-
-} // namespace Open62541ServerTests
