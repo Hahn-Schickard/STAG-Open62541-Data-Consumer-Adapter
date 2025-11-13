7ee401df7bb01f566994cd33650c1321e6389ec9
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2025-11-13 10:06:40 +0100

    refactor docu

--- includes/Utilities/VariantConverter.hpp
+++ includes/Utilities/VariantConverter.hpp
@@ -5,11 +5,37 @@
 #include <open62541/types.h>
 
 namespace open62541 {
+/**
+ * @brief Convert a given Information Model data type into a UA Type Reference
+ * node
+ *
+ * @attention Resulting UA_NodeId content must be cleared by the caller when the
+ * content is no longer needed
+ *
+ * @param type
+ * @return UA_NodeId
+ */
 UA_NodeId toNodeId(Information_Model::DataType type);
 
-/// The caller is responsible for calling `UA_Variant_clear` on the result
+/**
+ * @brief Convert a given Information Model variant into a UA_Variant
+ *
+ * @attention Resulting UA_Variant must be cleared by the caller, when content
+ * is no longer needed
+ *
+ * @param variant
+ * @return UA_Variant
+ */
 UA_Variant toUAVariant(const Information_Model::DataVariant& variant);
 
+/**
+ * @brief Convert a given UA_Variant into an Information Model variant
+ *
+ * @attention Given UA_Variant content is not cleared
+ *
+ * @param variant
+ * @return Information_Model::DataVariant
+ */
 Information_Model::DataVariant toDataVariant(const UA_Variant& variant);
 } // namespace open62541
 
